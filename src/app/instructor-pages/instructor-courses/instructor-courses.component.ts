aa07f1b50b3ae85b04866275a68d798caecf3e8f
MusicDev33 <smccowan33@gmail.com>
2020-05-11 21:03:50 -0600

    Added many icons

--- src/app/instructor-pages/instructor-courses/instructor-courses.component.ts
+++ src/app/instructor-pages/instructor-courses/instructor-courses.component.ts
@@ -29,7 +29,20 @@ export class InstructorCoursesComponent implements OnInit {
 
   createCourseMode: boolean;
 
-  availableIcons = ['fas fa-graduation-cap', 'fas fa-chalkboard', 'fas fa-bone', 'fas fa-skull'];
+  availableIcons = [
+    'fas fa-graduation-cap', 'fas fa-chalkboard', 'fas fa-bone', 'fas fa-skull', 'fas fa-flask', 'fas fa-sitemap',
+    'fas fa-dna', 'fas fa-paw', 'fas fa-network-wired', 'fas fa-database', 'fas fa-server', 'fas fa-tv',
+    'fas fa-robot', 'fas fa-prescription-bottle', 'fas fa-prescription-bottle-alt', 'fas fa-pills',
+    'fas fa-calculator', 'fas fa-superscript', 'fas fa-square-root-alt', 'fas fa-horse', 'fas fa-hat-cowboy-side',
+    'fas fa-industry', 'far fa-smile-beam', 'fas fa-wallet', 'fas fa-dollar-sign', 'fas fa-comments-dollar',
+    'fas fa-euro-sign', 'fas fa-pound-sign', 'fas fa-rupee-sign', 'fas fa-funnel-dollar', 'fas fa-brain',
+    'fas fa-user-tie', 'fas fa-chart-bar', 'fas fa-table', 'fas fa-briefcase', 'fas fa-seedling', 'fas fa-leaf',
+    'fas fa-tree', 'far fa-handshake', 'fas fa-wind', 'fas fa-globe', 'fas fa-globe-europe', 'fas fa-globe-americas',
+    'fas fa-globe-asia', 'fas fa-globe-africa', 'fas fa-water', 'fas fa-music', 'fas fa-headphones', 'fas fa-drum',
+    'fas fa-guitar', 'fas fa-palette', 'fas fa-paint-brush', 'fas fa-brush', 'fas fa-book', 'fas fa-book-open',
+    'fas fa-bookmark', 'fas fa-atlas', 'fas fa-vote-yea', 'fas fa-comment-dots', 'fas fa-utensils',
+    'fas fa-comments', 'fas fa-atom', 'fas fa-biohazard', 'fas fa-bezier-curve'
+  ];
 
   constructor(
     private route: ActivatedRoute,
