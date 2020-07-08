63786bbd7730ff62bc3b11b9d0116413865311f0
MusicDev33 <smccowan33@gmail.com>
2020-07-08 12:42:20 -0600

    co cookie

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -65,7 +65,7 @@ export class LoginComponent implements OnInit {
   sendLogin() {
     this.authService.sendLogin(this.loginField, this.loginPassField).subscribe((res: any) => {
       if (res.success) {
-        this.cookieService.set('jwt', res.jwt, 365, '/', null, true, 'None');
+        this.cookieService.set('jwt', res.jwt, 365, '/', 'inquantir.com', true, 'None');
         this.router.navigate(['/dashboard/courses']);
       }
     });
