a2ce0ee64cc6119d6dcdf34790d305c7b8a7a73d
MusicDev33 <smccowan33@gmail.com>
2020-08-19 15:48:12 -0600

    Can now select answers

--- src/app/components/q-answer/q-answer.component.ts
+++ src/app/components/q-answer/q-answer.component.ts
@@ -7,12 +7,36 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class QAnswerComponent implements OnInit {
 
+  // If anyone knows how to type this without ruining code quality,
+  // please tell me :'(
   @Input()
   layout: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.layout.type === 'multichoice') {
+      this.layout['selectedAnswer'] = '';
+    } else if (this.layout.type === 'checkbox') {
+      this.layout['selectedAnswers'] = [];
+    } else if (this.layout.type === 'essay' || this.layout.type === 'shortanswer') {
+      this.layout['userAnswer'] = '';
+    }
+  }
+
+  setAnswer(answer: any) {
+    if (this.layout.type === 'multichoice') {
+      this.layout.answers.forEach((qAnswer: any) => {
+        qAnswer['selected'] = false;
+      });
+      answer['selected'] = true;
+    } else if (this.layout.type === 'checkbox') {
+      if (answer.hasOwnProperty('selected')) {
+        answer.selected = !answer.selected;
+      } else {
+        answer['selected'] = true;
+      }
+    }
   }
 
 }
